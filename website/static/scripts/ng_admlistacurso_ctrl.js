f3cb7694826042624f10c321e31e5919eb940534
Filipe Assad Barauna <0118431@msmt.int>
2017-12-08 18:16:45 -0300

    Centralização dando defeito :(

--- website/static/scripts/ng_admlistacurso_ctrl.js
+++ website/static/scripts/ng_admlistacurso_ctrl.js
@@ -9,22 +9,41 @@ app.config(['$httpProvider', '$interpolateProvider',
     //$httpProvider.defaults.withCredentials = true;
 }]);
 
-app.controller('AdmListaCursoCtrl', function($window, $http, $rootScope){
+app.controller('AdmListaCursoCtrl', function($window, $http, $rootScope, $scope){
 	
-	this.lista = [];
+	$scope.lista = [];
+	this.selecionado = {};
 
 	$http({method: 'GET', url: urlServer + 'api/curso', withCredentials: true }).
 	        success(function(data, status, headers, config) {
 	            
 	            console.log(data);
-	            if(data != "Erro02"){	            	
-					this.this.lista = data;
-					console.log(this.lista);
+	            if(data != "Erro02"){
+	            	for(i=0; i < data.length; i++){
+	            		data[i].cla = "item-lista";
+	            		if(data[i].descricao.length > 40){
+	            			data[i].descricao = data[i].descricao.substring(0, 38) + "..."; 
+	            		}
+	            	}
+	            	$scope.lista = data;
 	            }
 
 	    	}).error(function(data, status, headers, config) {
 	        	console.log("Não Funcionou");	        	  
     		});
 
+	this.selecionaItem = function(item){
+
+		this.selecionado = item;
+
+		for(i=0; i < $scope.lista.length; i++){
+			if($scope.lista[i].id == item.id){
+				$scope.lista[i].cla = "item-lista-selecionado";
+			}else{
+				$scope.lista[i].cla = "item-lista";
+			}
+		}
+
+	}
 
 });
